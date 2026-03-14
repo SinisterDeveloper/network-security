719138f7665c11f1e257ac3978c9806e7a793290
SinisterDeveloper <krishaymanjunath@gmail.com>
2026-03-14 12:51:16 +0530

    feat: Initialise Hardhat setup

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  solidity: {
+    version: "0.8.28",
+  },
+  networks: {
+    amoy: {
+      type: "http",
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [
+        "0xca67da8fcbdfbffef18e2250350dad4a09d445713738ccfa03a9fc94b43b02a9",
+      ],
+      chainId: 80002,
+    },
+  },
+});
